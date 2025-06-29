eddf6eb7082a0c1503171ddaa9c003ab700a7317
Ryan Okonski <ryanguilherme2404@gmail.com>
2025-06-29 16:29:03 -0300

    M_BTTI16: Melhoria na main do projeto

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
+const PORT = process.env.PORT || 3000;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create<any>(AppModule, { cors: true });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
+  app.setGlobalPrefix('pcc');
+
+  await app.listen(PORT, () => {
+    console.log(`🚀 Backend está rodando na porta ${PORT}`);
+  });
 }
 bootstrap();
