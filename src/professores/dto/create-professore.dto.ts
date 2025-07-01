177e868fce9c04cdf61e3e6442b6aa7aeb6713e9
Valdeir de Souza Prestes <valdeirprestes@gmail.com>
2025-07-01 01:57:58 -0300

    Manter professor

---
+++ src/professores/dto/create-professore.dto.ts
@@ -0,0 +1,70 @@
+import {
+    IsString,
+    IsEmail,
+    IsNotEmpty,
+    IsOptional,
+    IsDate,
+    IsEnum,
+    isBoolean,
+    IsBoolean,
+    isInt,
+  } from 'class-validator';
+  import { AlunoProfessor, Qualificacao, SolicitacaoBancaProfessor } from '@prisma/client';
+  import { Type } from 'class-transformer';
+export class CreateProfessoreDto {
+    @IsString()
+    @IsNotEmpty()
+    nome_civil: string;
+
+    @IsString()
+    @IsNotEmpty()
+    cpf: string;
+
+    @IsEmail()
+    @IsNotEmpty()
+    email: string;
+
+    @IsString()
+    @IsNotEmpty()
+    formacao_origem: string;
+
+
+    @IsString()
+    @IsOptional()
+    area_atuacao: string;
+
+    @IsString()
+    @IsNotEmpty()
+    senha: string;
+
+    @IsString()
+    @IsNotEmpty()
+    titulacao:string;
+    
+    @IsString()
+    @IsOptional()
+    nome_social:string;
+
+    @IsString()
+    @IsOptional()
+    telefone:string;
+
+    @IsString()
+    @IsOptional()
+    curso_id: string;  
+
+    @IsString()
+    @IsOptional()
+    qualificacao: Qualificacao;
+
+    @IsBoolean()
+    pertence_uem: Boolean;
+
+
+    quantidade_orientandos: number;
+
+    aluno_professor: AlunoProfessor;
+    SolicitacaoBancaProfessor: SolicitacaoBancaProfessor;
+
+}
+    
