211097e2bdbad3ba71d03963e6d7cf9275973dcf
valdeirprestes <valdeirprestes@gmail.com>
2025-07-01 15:12:29 -0300

    Ajustando service professores

--- src/professores/professores.service.ts
+++ src/professores/professores.service.ts
@@ -1,26 +1,29 @@
 import { Injectable } from '@nestjs/common';
 import { CreateProfessoreDto } from './dto/create-professore.dto';
 import { UpdateProfessoreDto } from './dto/update-professore.dto';
+import { PrismaService } from 'src/service/prisma.service';
+import { Professor } from '@prisma/client';
 
 @Injectable()
 export class ProfessoresService {
-  create(createProfessoreDto: CreateProfessoreDto) {
-    return 'This action adds a new professore';
+  constructor (private prisma : PrismaService) {}
+  async create(createProfessoreDto: CreateProfessoreDto):Promise<Professor> {
+    return this.prisma.professor.create({data: createProfessoreDto});
   }
 
-  findAll() {
-    return `This action returns all professores`;
+  async findAll():Promise<Professor[]> {
+    return  this.prisma.professor.findMany();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} professore`;
+  async findOne(id: number) : Promise<Professor> {
+    return this.prisma.professor.findFirst();
   }
 
-  update(id: number, updateProfessoreDto: UpdateProfessoreDto) {
-    return `This action updates a #${id} professore`;
+  async update(id: string, updateProfessoreDto: UpdateProfessoreDto): Promise<Professor> {
+    return this.prisma.professor.update({data:updateProfessoreDto, where:{id}});
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} professore`;
+  async remove(id: string) {
+    return this.prisma.professor.delete({where:{id},});
   }
 }
