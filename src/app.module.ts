3fc406dcb5752b444974114af8f454df81d0f44c
Valdeir de Souza Prestes <valdeirprestes@gmail.com>
2025-07-01 22:07:39 -0300

    Ajuste de modulos de professores para professor

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProfessoresModule } from './professores/professores.module';
+import { ProfessorModule } from './professor/professor.module';
+
 
 @Module({
-  imports: [ProfessoresModule],
+  imports: [ProfessorModule],
   controllers: [AppController],
   providers: [AppService],
 })
