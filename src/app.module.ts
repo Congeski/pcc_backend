4fe4a09f9b5547005cbf4829e9d0c026243d41e5
Valdeir de Souza Prestes <valdeirprestes@gmail.com>
2025-07-25 00:04:13 -0300

    Fix: adding Student Crud, loading the Authentication module and accessing the Database in the src/app.modules.ts file

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProfessorModule } from './professor/professor.module';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { AlunoModule } from './aluno/aluno.module';
 
 
 @Module({
-  imports: [ProfessorModule],
+  imports: [AlunoModule,ProfessorModule, AuthModule,ConfigModule.forRoot({isGlobal:true})],
   controllers: [AppController],
   providers: [AppService],
 })
